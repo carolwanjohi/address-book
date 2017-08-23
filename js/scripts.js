8ab2c28258704d6d9cdeb7f61395d23ed29278e5
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-08-23 12:09:33 +0300

    Create address object. Create address object to get the contact's street, city and county. Add an array for addresses in the contact object.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,16 @@
 
 // Constructor 
 function Contact(first, last) {
-    this.firstName = first;
-    this.lastName = last;
-  }
+  this.firstName = first;
+  this.lastName = last;
+  this.addresses = [];
+}
+
+function Address(street, city, county) {
+  this.street = street;
+  this.city = city;
+  this.county = county;
+}
 
 Contact.prototype.fullName = function() {
     return this.firstName + " " + this.lastName;
