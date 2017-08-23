90636e70dbe02317f2c13b87a98eda1a287c95b0
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-08-23 13:11:23 +0300

    Functional Address system. Adding the address links and testing the code.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function Contact(first, last) {
   this.addresses = [];
 }
 
-function Address(street, city, county) {
+function Address(street, city, county) {  
   this.street = street;
   this.city = city;
   this.county = county;
@@ -57,15 +57,23 @@ $(document).ready(function() {
         
         newContact.addresses.push(newAddress);
       });
-
-      // $("input#new-first-name").val("");
-      // $("input#new-last-name").val("");
       
       $(".contact").last().click(function() {
         $("#show-contact").show();
         $("#show-contact h2").text(newContact.firstName);
         $(".first-name").text(newContact.firstName);
         $(".last-name").text(newContact.lastName);
+        $("ul#addresses").text("");
+        newContact.addresses.forEach(function(address) {
+          $("ul#addresses").append("<li>" + address.street + ", " + 
+            address.city + " " + address.County + "</li>");
+        });
       });
+
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input.new-street").val("");
+      $("input.new-city").val("");
+      $("input.new-county").val("");
     });
   });
